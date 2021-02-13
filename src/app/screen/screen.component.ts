64368f8727e8f08c8d8fcd510ab8f66cf443a1fe
patelshyam <patelshyam54@gmail.com>
2021-02-13 20:25:49 +0100

    Implemented Redefine functionality

--- src/app/screen/screen.component.ts
+++ src/app/screen/screen.component.ts
@@ -9,15 +9,39 @@ import { Subscription} from 'rxjs';
 })
 export class ScreenComponent implements OnInit {
 
+   NUMBER_OF_ARRAY_BARS = 300;
+
+   Array = [];
+
   clickEventSubscription:Subscription;
 
   constructor(private sharedService:SharedService) {
-    this.clickEventSubscription = this.sharedService.getClickEvent().subscribe((x)=>{
-      console.log(x);
+    this.resetArray();
+    this.clickEventSubscription = this.sharedService.getClickEvent().subscribe((shortType)=>{
+      console.log(shortType);
+      switch(shortType){
+        case "re-define":{
+          this.resetArray();
+        }
+      }
     })
    }
 
+   resetArray(){
+    const array = [];
+    for (let i = 0; i < this.NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(this.randomIntFromInterval(5, 730));
+    }
+    this.Array = array;
+    console.log(this.Array);
+   }
+
   ngOnInit() {
+  }
+
+  // From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+  randomIntFromInterval(min:any,max:any) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
 
