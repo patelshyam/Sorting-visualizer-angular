495f7db8d586bf6135216bb73fba5dc50b914d9d
patelshyam <patelshyam54@gmail.com>
2021-02-13 09:37:05 +0100

    Basic formate and structure implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ScreenComponent } from './screen/screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ScreenComponent
   ],
   imports: [
     BrowserModule
